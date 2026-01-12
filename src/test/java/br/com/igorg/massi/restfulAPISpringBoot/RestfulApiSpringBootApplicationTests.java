bd98b32a0de12d0ef78fe7be5ef0f28dc502a642
Igor Massi <igorg.massi@gmail.com>
2026-01-12 20:23:23 -0300

    First Steps in Java, Creating a Class Greetings To Use Rest

---
+++ src/test/java/br/com/igorg/massi/restfulAPISpringBoot/RestfulApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igorg.massi.restfulAPISpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
