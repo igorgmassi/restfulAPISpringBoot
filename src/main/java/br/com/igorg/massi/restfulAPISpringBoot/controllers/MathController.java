2d4db7713f58ad3661dd20daeda08f62ffe80a05
Igor Massi <igorg.massi@gmail.com>
2026-01-14 15:07:15 -0300

    Adjusting Pom.xml to project work again, and adding a MathController.

---
+++ src/main/java/br/com/igorg/massi/restfulAPISpringBoot/controllers/MathController.java
@@ -0,0 +1,36 @@
+package br.com.igorg.massi.restfulAPISpringBoot.controllers;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}/")
+    public Double sum(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+            )
+            throws IllegalArgumentException{
+        if(!isNumeric(numberOne) || isNumeric(numberTwo)) throw  new IllegalArgumentException();
+
+        return  convertToDouble(numberOne) + convertToDouble(numberTwo);
+
+    }
+
+    private Double convertToDouble( String strNumber) throws IllegalArgumentException{
+        if( strNumber.isEmpty()) throw new IllegalArgumentException();
+        String number = strNumber.replace(",", ".");
+
+        return Double.parseDouble(number);
+    }
+    private boolean isNumeric (String strNumber) throws IllegalArgumentException{
+        if( strNumber.isEmpty()) throw new IllegalArgumentException();
+        String number = strNumber.replace(",", ".");
+
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
